3b7ee42a2986a31a9b812bfc3295e6142bd8bc27
mahmut kaya <mahmutkaya@android-b112317571b0773d.localdomain>
2019-12-23 01:24:59 +0100

    new project (reactForm),
    download resume button

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react'
 import {Row} from 'reactstrap';
 import {projects} from '../logic/utilities'
 import portfolio from '../img/portfolio.PNG'
-import news from '../img/news.PNG'
+import news from '../img/news.jpg'
 import wiki from '../img/wiki.PNG'
 import todo from '../img/todo.PNG'
 import hyfrepo from '../img/hyfrepo.PNG'
+import reactForm from '../img/reactForm.jpg'
 
 class Projects extends Component {
   render() {
@@ -17,9 +18,10 @@ class Projects extends Component {
         <Row >
           {projects(portfolio, 'https://www.mahmutkaya.nl/')}
           {projects(news, 'https://news-api-2019.herokuapp.com/')}
-          {projects(wiki, 'https://codepen.io/mahmutkaya-the-selector/full/XoMmEW')}
+          {projects(wiki, 'https://wiki-search-vanilla-js.netlify.com/')}
           {projects(todo, 'https://todo-app-2018.netlify.com/')}
           {projects(hyfrepo, 'https://mahmutkaya.github.io/hyf-javascript3/week3/indexPromise.html')}
+          {projects(reactForm, 'https://grayscale-photos.netlify.com/')}
         </Row>
       </React.Fragment>
     )
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,13 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-class Resume extends Component{
-  render(){
-    return(
+class Resume extends Component {
+  render() {
+    return (
       <div>
         <h1>working on it...</h1>
+        <p>
+          <Link to="/files/CV - Mahmut Kaya.pdf" target="_blank" download>
+            click to download a pdf version of my resume.
+          </Link>
+        </p>
       </div>
-      )
-    }
+    );
   }
-  
-  export default Resume;  
+}
+
+export default Resume;
